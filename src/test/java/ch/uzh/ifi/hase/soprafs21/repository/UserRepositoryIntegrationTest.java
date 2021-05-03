bb652967b853cb1c76d20c37f51bad981f1f6b70
Krzysztof <krzysztof.wroblewski@uzh.ch>
2021-05-03 11:40:08 +0200

    Revert "Test"
    This reverts commit 4d670fe6c8b045501757f99a4bfa926a4f3bd8cf.

--- src/test/java/ch/uzh/ifi/hase/soprafs21/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/repository/UserRepositoryIntegrationTest.java
@@ -11,41 +11,41 @@ import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//@DataJpaTest
-//public class UserRepositoryIntegrationTest {
-//
-//    @Autowired
-//    private TestEntityManager entityManager;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Test
-//    public void findByUsername_success() {
-//        // given
-//        Date currentDate = new Date();
-//        User user = new User();
-//        user.setUsername("firstname@lastname");
-//        user.setStatus(UserStatus.OFFLINE);
-//        user.setToken("1");
-//        user.setMail("mail@mail.com");
-//        user.setPassword("***");
-//        user.setCreationDate(currentDate);
-//
-//        entityManager.persist(user);
-//        entityManager.flush();
-//
-//        // when
-//        User found = userRepository.findByUsername(user.getUsername());
-//
-//        // then
-//        assertNotNull(found.getId());
-//        assertEquals(found.getUsername(), user.getUsername());
-//        assertEquals(found.getToken(), user.getToken());
-//        assertEquals(found.getStatus(), user.getStatus());
-//        assertEquals(found.getCreationDate(), user.getCreationDate());
-//        assertEquals(found.getMail(), user.getMail());
-//        assertEquals(found.getPassword(), user.getPassword());
-//    }
-//}
+
+@DataJpaTest
+public class UserRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void findByUsername_success() {
+        // given
+        Date currentDate = new Date();
+        User user = new User();
+        user.setUsername("firstname@lastname");
+        user.setStatus(UserStatus.OFFLINE);
+        user.setToken("1");
+        user.setMail("mail@mail.com");
+        user.setPassword("***");
+        user.setCreationDate(currentDate);
+
+        entityManager.persist(user);
+        entityManager.flush();
+
+        // when
+        User found = userRepository.findByUsername(user.getUsername());
+
+        // then
+        assertNotNull(found.getId());
+        assertEquals(found.getUsername(), user.getUsername());
+        assertEquals(found.getToken(), user.getToken());
+        assertEquals(found.getStatus(), user.getStatus());
+        assertEquals(found.getCreationDate(), user.getCreationDate());
+        assertEquals(found.getMail(), user.getMail());
+        assertEquals(found.getPassword(), user.getPassword());
+    }
+}
