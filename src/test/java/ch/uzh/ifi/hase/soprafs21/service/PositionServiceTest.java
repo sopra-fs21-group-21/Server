945f47b0f05f40468c93e324d5476ca98e9436d8
alessandrovanzo <vanzo.alessandro@hotmail.it>
2021-05-23 23:48:15 +0200

    Fixed and improved positionService tests.
    closes #70

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/PositionServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/PositionServiceTest.java
@@ -40,26 +40,46 @@ public class PositionServiceTest {
         testPosition.setAmount(BigDecimal.valueOf(10));
         testPosition.setCurrency("CHF");
         testPosition.setOpeningPrice(BigDecimal.valueOf(100));
+        testPosition.setType(PositionType.STOCK_LONG);
 
         // when -> any object is being save in the userRepository -> return the dummy testUser
         Mockito.when(positionRepository.saveAndFlush(Mockito.any())).thenReturn(testPosition);
     }
-//
-//    @Test
-//    public void openPosition_validInput() {
-//        // when -> any object is being saved in the positionRepository -> return the dummy testUser
-//
-//        Position createdPosition = positionService.openPosition(testPosition);
-//
-//        // then
-//        Mockito.verify(positionRepository, Mockito.times(1)).saveAndFlush(Mockito.any());
-//
-//
-//        assertEquals(testPosition.getId(), createdPosition.getId());
-//        assertEquals(testPosition.getCode(), createdPosition.getCode());
-//        assertEquals(testPosition.getPrice(), createdPosition.getPrice());
-//        assertEquals(testPosition.getOpeningPrice(), createdPosition.getOpeningPrice());
-//    }
+
+
+    @Test
+    public void openPosition_validInput_Long() {
+        // when -> any object is being saved in the positionRepository -> return the dummy testUser
+
+        Position createdPosition = positionService.openPosition(testPosition);
+        // then
+        Mockito.verify(positionRepository, Mockito.times(1)).saveAndFlush(Mockito.any());
+        Mockito.doReturn(testPosition).when(positionRepository).saveAndFlush(Mockito.any());
+
+
+        assertEquals(testPosition.getId(), createdPosition.getId());
+        assertEquals(testPosition.getCode(), createdPosition.getCode());
+        assertEquals(testPosition.getPrice(), createdPosition.getPrice());
+        assertEquals(testPosition.getOpeningPrice(), createdPosition.getOpeningPrice());
+    }
+
+    @Test
+    public void openPosition_validInput_Short() {
+        // when -> any object is being saved in the positionRepository -> return the dummy testUser
+
+        testPosition.setType(PositionType.STOCK_SHORT);
+
+        Position createdPosition = positionService.openPosition(testPosition);
+        // then
+        Mockito.verify(positionRepository, Mockito.times(1)).saveAndFlush(Mockito.any());
+        Mockito.doReturn(testPosition).when(positionRepository).saveAndFlush(Mockito.any());
+
+
+        assertEquals(testPosition.getId(), createdPosition.getId());
+        assertEquals(testPosition.getCode(), createdPosition.getCode());
+        assertEquals(testPosition.getPrice(), createdPosition.getPrice());
+        assertEquals(testPosition.getOpeningPrice(), createdPosition.getOpeningPrice());
+    }
 
     @Test
     public void openPosition_nullCode_throwsException() {
